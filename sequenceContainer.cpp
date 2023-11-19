f3d78f1918c182c08fd034b8b8c31b326f8bcadf
AndreyMustafin2001 <150649500+AndreyMustafin2001@users.noreply.github.com>
2023-11-19 13:52:12 +0400

    N1

--- sequenceContainer.cpp
+++ sequenceContainer.cpp
@@ -6,7 +6,7 @@
 #include <list>
 
 void Print(auto cont,char S) {
-    for(auto iter = cont.begin(); iter != cont.end(); ++iter)
+    for (auto iter = cont.begin(); iter != cont.end(); ++iter)
     {
      auto iter2 = iter;
           iter2++;
@@ -24,8 +24,8 @@ int main()
   std::list <int> l = {1,2,3,4};
   std::vector <int> v = {5,6,7,8};
   std::string ABC = "Salam";
-  std::array <int,5> mas ={9,10,11,12};
-  std::deque <double> Dq ={13.3,14.4,15.5,16.9};
+  std::array <int,5> mas = {9,10,11,12};
+  std::deque <double> Dq = {13.3,14.4,15.5,16.9};
 
   Print(l,',');
   Print(v,',');
